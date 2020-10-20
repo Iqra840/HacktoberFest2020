3932f9927c8169edbb546f2dbbc199589468120c
Iqra <iqra@Iqras-MacBook-Air.local>
2020-10-20 19:12:17 +0500

    added documentation for better understanding of the code

--- Basic Programs/leap_year.py
+++ Basic Programs/leap_year.py
@@ -6,12 +6,12 @@ year = 2000
 # year = int(input("Enter a year: "))
 
 if (year % 4) == 0:
-#If a year is multiple of 400, then it is a leap year
+#If a year is multiple of 4, then it is a leap year
     if (year % 100) == 0:
     #If a year is muliple of 100, then it is a leap year
        if (year % 400) == 0:
+        #If a year is multiple of 400, then it is a leap year
            print("{0} is a leap year".format(year))
-           #If a year is multiple of 400, then it is a leap year
        else:
            print("{0} is not a leap year".format(year))
    else:
